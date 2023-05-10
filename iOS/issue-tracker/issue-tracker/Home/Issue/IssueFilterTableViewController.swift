2a2dc74a827b3febf770785b4bc95a12710cfe9f
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-10 16:37:49 +0900

    Feat: Table view 및 view controller 추가

---
+++ iOS/issue-tracker/issue-tracker/Home/Issue/IssueFilterTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  IssueFilterTableViewController.swift
+//  issue-tracker
+//
+//  Created by 에디 on 2023/05/10.
+//
+
+import UIKit
+
+class IssueFilterTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0: return 1
+        case 1: return 1
+        case 2: return 1
+        default: return 1
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+         
+
+        return cell
+    }
+}
