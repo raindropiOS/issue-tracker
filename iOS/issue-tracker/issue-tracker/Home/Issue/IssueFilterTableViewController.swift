b69aeda8eb62ac416643881b89c183423400dc99
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-11 10:51:15 +0900

    Feat: Issue filter 테이블에 임시 header 추가

--- iOS/issue-tracker/issue-tracker/Home/Issue/IssueFilterTableViewController.swift
+++ iOS/issue-tracker/issue-tracker/Home/Issue/IssueFilterTableViewController.swift
@@ -16,23 +16,40 @@ class IssueFilterTableViewController: UITableViewController {
         tableView.rowHeight = tableView.frame.width * 0.1173
     }
 
-    // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
-        case 0: return 1
-        case 1: return 1
-        case 2: return 1
+        case 0: return 2
+        case 1: return 3
+        case 2: return 4
         default: return 1
         }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "filterOptionCell", for: indexPath) as? IssueFilterTableViewCell else { return UITableViewCell() }
-        cell.configure(withOptionName: "filterOption")
+        cell.setOptionName(with: "filterOption")
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if let cell = tableView.cellForRow(at: indexPath) as? IssueFilterTableViewCell {
+            cell.toggleCheckmarkColor()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection
+                                section: Int) -> String? {
+        switch section {
+        case 0: return "상태"
+        case 1: return "담당자"
+        case 2: return "레이블"
+        default: return "Header \(section)"
+        }
     }
 }
