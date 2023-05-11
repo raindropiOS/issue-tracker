f052d02947000e05f8c2d9aff1ba591977f44b2a
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-11 10:50:05 +0900

    Feat: Issue filter cell 기능 추가
    - 터치시 checkmark 색상 변경
    - 초기화시 checkmark 색상을 결정하는 메소드 추가

--- iOS/issue-tracker/issue-tracker/Home/Issue/IssueFilterTableViewCell.swift
+++ iOS/issue-tracker/issue-tracker/Home/Issue/IssueFilterTableViewCell.swift
@@ -11,27 +11,37 @@ class IssueFilterTableViewCell: UITableViewCell {
 
     @IBOutlet var filterOptionLabel: UILabel!
     @IBOutlet var checkmarkView: UIImageView!
+    let config = UIImage.SymbolConfiguration(pointSize: 14)
+    var isOptionSelected = false
+    lazy var tintCheckmarkImage = UIImage(systemName: "checkmark", withConfiguration: config)
+    lazy var grayCheckmarkImage = tintCheckmarkImage?.withTintColor(.gray, renderingMode: .alwaysOriginal)
+    
     override func awakeFromNib() {
-        
         super.awakeFromNib()
-        // Initialization code
-//        let config = UIImage.SymbolConfiguration(scale: .small)
-        let config = UIImage.SymbolConfiguration(pointSize: 14)
-        let image = UIImage(systemName: "checkmark", withConfiguration: config)?.withTintColor(.gray, renderingMode: .alwaysOriginal)
-//        image?.size = CGSize(width: 3, height: 3)
-        checkmarkView.image = image
-
-        
+        configureAsNotChecked()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureAsNotChecked() {
+        checkmarkView.image = grayCheckmarkImage
     }
     
-    func configure(withOptionName optionName : String) {
+    func configureAsChecked() {
+        checkmarkView.image = tintCheckmarkImage
+    }
+    
+    func setOptionName(with optionName : String) {
         self.filterOptionLabel.text = optionName
     }
     
+    func toggleCheckmarkColor() {
+        if isOptionSelected {
+            checkmarkView.image = tintCheckmarkImage
+        } else {
+            checkmarkView.image = grayCheckmarkImage
+        }
+        
+        isOptionSelected = !isOptionSelected
+    }
+    
 }
+
