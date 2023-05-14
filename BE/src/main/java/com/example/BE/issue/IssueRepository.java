54c5b831769b100addb08ba3b89d52738db2be8c
CiRe <dltpwns6@naver.com>
2023-05-14 22:41:19 +0900

    Refactor: 데이터베이스에서 모든 이슈에 대한 정보를 조회하는 기능 리펙토링

--- BE/src/main/java/com/example/BE/issue/IssueRepository.java
+++ BE/src/main/java/com/example/BE/issue/IssueRepository.java
@@ -1,6 +1,7 @@
 package com.example.BE.issue;
 
 import com.example.BE.dto.show.issue.detailed.*;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -71,33 +72,16 @@ public class IssueRepository {
         return template.queryForObject(sql,new HashMap<>(), Integer.class);
     }
     private RowMapper<FeSimpleIssue> feSimpleIssueRowMapper = (rs, rowNum) -> {
-        FeSimpleIssue issue = new FeSimpleIssue();
-        int issueNumber = rs.getInt("number");
-        String issueTitle = rs.getString("title");
-        boolean issueState = rs.getBoolean("state");
-        LocalDateTime issueCreatedDate = rs.getObject("created_date", LocalDateTime.class);
-        String milestoneName = rs.getString("milestone_name");
-        String userNickName = rs.getString("nickname");
-        String userImageUrl = rs.getString("imageUrl");
-        String labelName = rs.getString("label_name");
-        String labelBackgroundColor = rs.getString("background_color");
-        String labelTextColor = rs.getString("text_color");
+        FeSimpleIssue feSimpleIssue = new BeanPropertyRowMapper<>(FeSimpleIssue.class).mapRow(rs, rowNum);
+        SimpleMilestone simpleMilestone = new BeanPropertyRowMapper<>(SimpleMilestone.class).mapRow(rs, rowNum);
+        SimpleAuthor simpleAuthor = new BeanPropertyRowMapper<>(SimpleAuthor.class).mapRow(rs, rowNum);
+        SimpleLabel simpleLabel = new BeanPropertyRowMapper<>(SimpleLabel.class).mapRow(rs, rowNum);
 
-        SimpleAuthor simpleAuthor = new SimpleAuthor(userNickName, userImageUrl);
-        SimpleMilestone simpleMilestone = new SimpleMilestone(milestoneName);
-        SimpleLabel simpleLabel = new SimpleLabel(labelName, labelTextColor, labelBackgroundColor);
+        feSimpleIssue.setMilestone(simpleMilestone);
+        feSimpleIssue.setAuthor(simpleAuthor);
+        feSimpleIssue.addLabel(simpleLabel);
 
-        issue.setNumber(issueNumber);
-        issue.setTitle(issueTitle);
-        issue.setCreatedDate(issueCreatedDate);
-        issue.setState(issueState);
-        issue.setAuthor(simpleAuthor);
-        issue.addLabel(simpleLabel);
-        issue.setMilestone(simpleMilestone);
-
-        return issue;
+        return feSimpleIssue;
     };
-
-
 
 }
