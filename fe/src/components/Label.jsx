3db5bcbb68a937a676d7a7aa45a31c39df410a29
sjuhan123 <sjuhan123@gmail.com>
2023-05-16 16:48:38 +0900

    Feat: common Label 컴포넌트 1차 구현 완료

---
+++ fe/src/components/Label.jsx
@@ -0,0 +1,43 @@
+import styled, { css } from 'styled-components';
+
+const Label = ({ name, ...colors }) => {
+  return (
+    <LableBox name={name} {...colors}>
+      {name}
+    </LableBox>
+  );
+};
+
+export default Label;
+
+const LableBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 24px;
+  font-weight: 500;
+  font-size: 12px;
+  color: ${(p) => p.text_color};
+  background-color: ${(p) => p.background_color};
+  border-radius: 50px;
+
+  ${({ name }) => {
+    const nameLength = name.length;
+    const minWidth = 32;
+
+    if (nameLength <= 4) {
+      return css`
+        width: ${minWidth}px;
+        padding-left: 16px;
+        padding-right: 16px;
+      `;
+    }
+    const dynamicWidth = nameLength * 8 + 16;
+    return css`
+      width: ${dynamicWidth}px;
+      padding-left: 16px;
+      padding-right: 16px;
+    `;
+  }}
+`;
