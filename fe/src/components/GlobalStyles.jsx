7ad92bd081ac1618c643d108d1f54c82d4459256
sjuhan123 <sjuhan123@gmail.com>
2023-05-16 16:47:47 +0900

    Style: GlobalStyles 불피요한 속성 제거, 필요한 속성 추가

--- fe/src/components/GlobalStyles.jsx
+++ fe/src/components/GlobalStyles.jsx
@@ -1,25 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle` 
-    html {
-    font-size: 62.5%;
+    a {
+      text-decoration: none;
+      color: inherit;
     }
-    a{
-        text-decoration: none;
-        color: inherit;
+
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
     }
-    *{
-        box-sizing: border-box;
-    }
-    input, textarea { 
-      -moz-user-select: auto;
-      -webkit-user-select: auto;
-      -ms-user-select: auto;
-      user-select: auto;
-    }
-    input:focus {
-      outline: none;
-    }
+
     button {
       border: none;
       background: none;
