5e1ae958ef8c56fe610351a65902c61a2009bd01
sjuhan123 <sjuhan123@gmail.com>
2023-05-17 15:51:34 +0900

    Feat: 이슈 테이블 필터 드랍다운 기능 추가

--- fe/src/components/IssueTable/TableToolBar/TableFilterButtons/TableFilterButtons.jsx
+++ fe/src/components/IssueTable/TableToolBar/TableFilterButtons/TableFilterButtons.jsx
@@ -1,17 +1,72 @@
 import styled from 'styled-components';
 import chevronDown from '../../../../assets/chevronDown.svg';
+import DropDown from '../../../DropDown/DropDown';
 
 const TableFilterButtons = () => {
-  const filterTypes = ['담당자', '레이블', '마일스톤', '작성자'];
+  const assigneeDropDownConfig = {
+    buttonText: '담당자',
+    buttonImage: chevronDown,
+    headerText: '담당자 필터',
+    bodyItems: [
+      { text: '담당자가 없는 이슈', checked: false },
+      { img: chevronDown, text: 'chloe', checked: false },
+      { img: chevronDown, text: 'head', checked: false },
+      { img: chevronDown, text: 'sam', checked: false },
+      { img: chevronDown, text: 'zello', checked: false },
+    ],
+    bodyCheck: true,
+    posright: true,
+    marginTop: '10px',
+  };
+
+  const labelDropDownConfig = {
+    buttonText: '레이블',
+    buttonImage: chevronDown,
+    headerText: '레이블 필터',
+    bodyItems: [
+      { text: '레이블이 없는 이슈', checked: false },
+      { img: chevronDown, text: 'documentation', checked: false },
+      { img: chevronDown, text: 'bug', checked: false },
+    ],
+    bodyCheck: true,
+    posright: true,
+    marginTop: '10px',
+  };
+
+  const milestoneDropDownConfig = {
+    buttonText: '마일스톤',
+    buttonImage: chevronDown,
+    headerText: '마일스톤 필터',
+    bodyItems: [
+      { text: '마일스톤이 없는 이슈', checked: false },
+      { text: '그룹프로젝트: 이슈트레커', checked: false },
+    ],
+    bodyCheck: true,
+    posright: true,
+    marginTop: '10px',
+  };
+
+  const authorDropDownConfig = {
+    buttonText: '작성자',
+    buttonImage: chevronDown,
+    headerText: '작성자 필터',
+    bodyItems: [
+      { img: chevronDown, text: 'chloe', checked: false },
+      { img: chevronDown, text: 'head', checked: false },
+      { img: chevronDown, text: 'sam', checked: false },
+      { img: chevronDown, text: 'zello', checked: false },
+    ],
+    bodyCheck: true,
+    posright: true,
+    marginTop: '10px',
+  };
 
   return (
     <TableFilterButtonsBox>
-      {filterTypes.map((type) => (
-        <TableFilterButton key={type}>
-          {type}
-          <img src={chevronDown} alt="chevronDown" />
-        </TableFilterButton>
-      ))}
+      <DropDown config={assigneeDropDownConfig} />
+      <DropDown config={labelDropDownConfig} />
+      <DropDown config={milestoneDropDownConfig} />
+      <DropDown config={authorDropDownConfig} />
     </TableFilterButtonsBox>
   );
 };
@@ -20,16 +75,7 @@ export default TableFilterButtons;
 
 const TableFilterButtonsBox = styled.div`
   display: flex;
-
   gap: 36px;
-  color: #6e7191;
-`;
 
-const TableFilterButton = styled.div`
-  display: flex;
-
-  font-weight: 700;
-  font-size: 16px;
-  gap: 8px;
-  cursor: pointer;
+  color: ${({ theme }) => theme.color.neutralTextWeak};
 `;
