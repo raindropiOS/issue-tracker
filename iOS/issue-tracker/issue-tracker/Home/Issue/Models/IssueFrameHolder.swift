a49a982369c44fc3b2d39c2416195861c80f9802
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-20 22:06:42 +0900

    Feat: IssueFrameHolder, NetworkManager 추가

---
+++ iOS/issue-tracker/issue-tracker/Home/Issue/Models/IssueFrameHolder.swift
@@ -0,0 +1,12 @@
+//
+//  IssueFrameHolder.swift
+//  issue-tracker
+//
+//  Created by 에디 on 2023/05/20.
+//
+
+import Foundation
+
+struct IssueFrameHolder: Codable {
+    let issues: [IssueFrame]
+}
