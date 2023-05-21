a9e728faddf5211ee231f8e1c1bec4b557612cdb
Hyun <ghkdgus29@naver.com>
2023-05-21 10:15:31 +0900

    Rename: MilestonRepository -> MilestoneRepository 오타 교정

--- be/src/main/java/com/example/be/milestone/MilestoneRepository.java
+++ be/src/main/java/com/example/be/milestone/MilestoneRepository.java
@@ -2,7 +2,7 @@ package com.example.be.milestone;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface MilestonRepository extends CrudRepository<Milestone, String> {
+public interface MilestoneRepository extends CrudRepository<Milestone, String> {
 
     Milestone findByName(String name);
 }
