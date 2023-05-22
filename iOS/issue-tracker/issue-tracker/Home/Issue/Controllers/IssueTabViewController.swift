877b9c7b9722ce267737b9fa827485e33215a344
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-22 18:05:07 +0900

    Feat: IssueTabViewController fetchData 메소드 추가
    - viewDidLoad에서 data를 가져오는 동작 메소드로 변경

--- iOS/issue-tracker/issue-tracker/Home/Issue/Controllers/IssueTabViewController.swift
+++ iOS/issue-tracker/issue-tracker/Home/Issue/Controllers/IssueTabViewController.swift
@@ -19,6 +19,7 @@ class IssueTabViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         backPlane.addSubview(collectionView)
+        // TODO: fetchData(with: String)로 대체 예정
         networkManager.fetchIssueData { result in
             switch result {
             case .success(let issueFrameHolder):
@@ -37,6 +38,23 @@ class IssueTabViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let navigationController = segue.destination as? UINavigationController{
             let filterTableViewController = navigationController.topViewController as? IssueFilterTableViewController
+        }
+    }
+    
+    func fetchData(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        networkManager.fetchIssueData(with: url) { result in
+            switch result {
+            case .success(let issueFrameHolder):
+                self.issueFrames = issueFrameHolder.issues
+                guard let issueFrames = self.issueFrames else { return }
+                self.collectionView.issueFrames = issueFrames
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            case .failure(let error):
+                self.logger.error("error : \(error)")
+            }
         }
     }
 }
