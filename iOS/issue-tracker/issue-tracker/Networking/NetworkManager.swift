c80040193868739c19584ac7747b9406838fd9a3
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-22 18:01:49 +0900

    Feat: NetworkManger fetchIssueData(with url: URL, completion:) 메소드 추가
    - URL 받아 data를 fetch할 수 있도록 추가

--- iOS/issue-tracker/issue-tracker/Networking/NetworkManager.swift
+++ iOS/issue-tracker/issue-tracker/Networking/NetworkManager.swift
@@ -8,6 +8,27 @@
 import Foundation
 
 class NetworkManager {
+    func fetchIssueData(
+        with url: URL,
+        completion: @escaping (Result<IssueFrameHolder, Error>) -> Void) {
+            let request = URLRequest(url: url, timeoutInterval: 3.0)
+            
+            URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    completion(.failure(error))
+                }
+                
+                if let data = data {
+                    do {
+                        let issueFrames = try JSONDecoder().decode(IssueFrameHolder.self, from: data)
+                        completion(.success(issueFrames))
+                    } catch let decodeError {
+                        completion(.failure(decodeError))
+                    }
+                }
+            }.resume()
+    }
+    
     func fetchIssueData(completion: @escaping (Result<IssueFrameHolder, Error>) -> Void) {
         guard let url = URL(string: "http://3.38.73.117:8080/api-ios/issues") else { return }
         let request = URLRequest(url: url, timeoutInterval: 3.0)
@@ -25,7 +46,6 @@ class NetworkManager {
                     completion(.failure(decodeError))
                 }
             }
-        }.resume()
-        
+        }.resume() 
     }
 }
