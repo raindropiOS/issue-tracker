0c28314017dd5786af7ae7db20fa368db15e9925
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-23 10:53:26 +0900

    Feat: FilterOption 타입 추가

---
+++ iOS/issue-tracker/issue-tracker/Home/Issue/Models/FilterOption.swift
@@ -0,0 +1,13 @@
+//
+//  FilterOption.swift
+//  issue-tracker
+//
+//  Created by 에디 on 2023/05/23.
+//
+
+import Foundation
+
+struct FilterOption {
+    let filterLabel: String
+    let filterUrlStr: String?
+}
