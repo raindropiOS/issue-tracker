4894386d149208815397319b88705fd5ee0b0154
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-24 14:59:04 +0900

    Style: IssueFilterTableViewController 불필요 comma 삭제

--- iOS/issue-tracker/issue-tracker/Home/Issue/Controllers/IssueFilterTableViewController.swift
+++ iOS/issue-tracker/issue-tracker/Home/Issue/Controllers/IssueFilterTableViewController.swift
@@ -101,18 +101,18 @@ struct FilterOptionListMock: FilterOptionsLike {
             FilterOption(filterLabel: "열린 이슈", filterUrlStr: nil),
             FilterOption(filterLabel: "내가 작성한 이슈", filterUrlStr: nil),
             FilterOption(filterLabel: "내가 댓글을 남긴 이슈", filterUrlStr: nil),
-            FilterOption(filterLabel: "닫힌 이슈", filterUrlStr: "{close=true}", isSelected: false),
+            FilterOption(filterLabel: "닫힌 이슈", filterUrlStr: "{close=true}", isSelected: false)
         ],
         [
             FilterOption(filterLabel: "chloe", filterUrlStr: nil),
             FilterOption(filterLabel: "head", filterUrlStr: nil),
             FilterOption(filterLabel: "sam", filterUrlStr: nil),
-            FilterOption(filterLabel: "zello", filterUrlStr: nil),
+            FilterOption(filterLabel: "zello", filterUrlStr: nil)
         ],
         [
             FilterOption(filterLabel: "레이블 없음", filterUrlStr: nil, isSelected: false),
-            FilterOption(filterLabel: "그룹프로젝트:이슈트래커", filterUrlStr: nil, isSelected: false),
-        ],
+            FilterOption(filterLabel: "그룹프로젝트:이슈트래커", filterUrlStr: nil, isSelected: false)
+        ]
     ]
     
     func collectSelectedFilterOptionUrlString() -> String {
