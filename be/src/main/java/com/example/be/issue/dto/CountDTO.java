5537cd617b6a3c9a68b20b69a805022abe6189d2
CiRe <dltpwns6@naver.com>
2023-05-24 11:28:45 +0900

    Revert "Merge branch 'develop-BE' of github.com:sjuhan123/issue-tracker into develop-BE"
    This reverts commit 29f229b749a3f4a4983d6228f6711226e3dbaa16, reversing
    changes made to 6b46436856d2bcf77b41015d171c8af3d9f9650b.

--- be/src/main/java/com/example/be/issue/dto/CountDTO.java
+++ be/src/main/java/com/example/be/issue/dto/CountDTO.java
@@ -4,13 +4,17 @@ public class CountDTO {
 
     private int openedIssuesCount;
     private int closedIssuesCount;
+    private int labelsCount;
+    private int milestoneCount;
 
     public CountDTO() {
     }
 
-    public CountDTO(int openedIssuesCount, int closedIssuesCount) {
+    public CountDTO(int openedIssuesCount, int closedIssuesCount, int labelsCount, int milestoneCount) {
         this.openedIssuesCount = openedIssuesCount;
         this.closedIssuesCount = closedIssuesCount;
+        this.labelsCount = labelsCount;
+        this.milestoneCount = milestoneCount;
     }
 
     public int getOpenedIssuesCount() {
@@ -27,5 +31,21 @@ public class CountDTO {
 
     public void setClosedIssuesCount(int closedIssuesCount) {
         this.closedIssuesCount = closedIssuesCount;
+    }
+
+    public int getLabelsCount() {
+        return labelsCount;
+    }
+
+    public void setLabelsCount(int labelsCount) {
+        this.labelsCount = labelsCount;
+    }
+
+    public int getMilestoneCount() {
+        return milestoneCount;
+    }
+
+    public void setMilestoneCount(int milestoneCount) {
+        this.milestoneCount = milestoneCount;
     }
 }
--- be/src/main/java/com/example/be/issue/dto/FeIssueResponseDTO.java
+++ be/src/main/java/com/example/be/issue/dto/FeIssueResponseDTO.java
@@ -1,39 +1,32 @@
 package com.example.be.issue.dto;
 
 import com.example.be.issue.Issue;
-import com.example.be.label.Label;
-import com.example.be.milestone.Milestone;
 
 import java.util.Collection;
-import java.util.List;
 
 public class FeIssueResponseDTO {
 
     private Collection<Issue> issues;
     private CountDTO counts;
-    private List<Label> allLabels;
-    private List<Milestone> allMilestones;
 
-    public FeIssueResponseDTO(Collection<Issue> issues, CountDTO counts, List<Label> allLabels, List<Milestone> allMilestones) {
+    public FeIssueResponseDTO(Collection<Issue> issues, CountDTO counts) {
         this.issues = issues;
         this.counts = counts;
-        this.allLabels = allLabels;
-        this.allMilestones = allMilestones;
     }
 
     public Collection<Issue> getIssues() {
         return issues;
     }
 
+    public void setIssues(Collection<Issue> issues) {
+        this.issues = issues;
+    }
+
     public CountDTO getCounts() {
         return counts;
     }
 
-    public List<Label> getAllLabels() {
-        return allLabels;
-    }
-
-    public List<Milestone> getAllMilestones() {
-        return allMilestones;
+    public void setCounts(CountDTO counts) {
+        this.counts = counts;
     }
 }
