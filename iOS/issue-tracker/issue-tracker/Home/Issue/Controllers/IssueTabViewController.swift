d2f84b48ca8ddb255f63ec48cfc05461c76df9d7
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-24 16:43:44 +0900

    Refactor: fetchIssueData -> fetchData로 메소드 변경

--- iOS/issue-tracker/issue-tracker/Home/Issue/Controllers/IssueTabViewController.swift
+++ iOS/issue-tracker/issue-tracker/Home/Issue/Controllers/IssueTabViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 import OSLog
 
 class IssueTabViewController: UIViewController {
-    
     @IBOutlet var filterButton: UIBarButtonItem!
     @IBOutlet var selectButton: UIBarButtonItem!
     @IBOutlet var collectionView: IssueCollectionView!
@@ -27,21 +26,11 @@ class IssueTabViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: fetchData(with: String)로 대체 예정
-        networkManager.fetchIssueData { result in
-            switch result {
-            case .success(let issueFrameHolder):
-                self.issueFrames = issueFrameHolder.issues
-                guard let issueFrames = self.issueFrames else { return }
-                self.collectionView.issueFrames = issueFrames
-                DispatchQueue.main.async {
-                    self.collectionView.reloadData()
-                }
-            case .failure(let error):
-                self.logger.error("error : \(error)")
-            }
-        }
         setCancelButton()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchData()
     }
     
     private func setCancelButton() {
@@ -54,7 +43,6 @@ class IssueTabViewController: UIViewController {
     }
     
     @IBAction func selectButtonTouched(_ sender: UIButton) {
-        
         self.navigationController?.isToolbarHidden = false
         self.tabBarController?.tabBar.isHidden = true
         self.navigationItem.leftBarButtonItem = nothingButton
@@ -69,13 +57,12 @@ class IssueTabViewController: UIViewController {
         self.navigationItem.rightBarButtonItem = selectButton
     }
     
-    func fetchData() {
+    private func fetchData() {
         guard let url = URL(string: currentIssueDataUrlString) else {
             self.logger.log(
                 "Invalie URL string : \(self.currentIssueDataUrlString)")
             return
         }
-        
         networkManager.fetchIssueData(with: url) { result in
             switch result {
             case .success(let issueFrameHolder):
@@ -95,5 +82,3 @@ class IssueTabViewController: UIViewController {
         currentIssueDataUrlString = urlString
     }
 }
-
-
