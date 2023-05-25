1ad78f1b94f5c7bde1274445693a9848f79bf37f
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-25 11:50:04 +0900

    Feat: Issue 필터 기능 추가

--- iOS/issue-tracker/issue-tracker/Home/Issue/Models/FilterOptionListMock.swift
+++ iOS/issue-tracker/issue-tracker/Home/Issue/Models/FilterOptionListMock.swift
@@ -10,32 +10,46 @@ import Foundation
 class FilterOptionListMock: FilterOptionsLike {
     var list: [[FilterOption]] = [
         [
-            FilterOption(filterLabel: "열린 이슈", filterUrlStr: nil),
+            FilterOption(filterLabel: "열린 이슈", filterUrlStr: "state=true"),
             FilterOption(filterLabel: "내가 작성한 이슈", filterUrlStr: nil),
             FilterOption(filterLabel: "내가 댓글을 남긴 이슈", filterUrlStr: nil),
-            FilterOption(filterLabel: "닫힌 이슈", filterUrlStr: "{close=true}", isSelected: false)
+            FilterOption(filterLabel: "닫힌 이슈", filterUrlStr: "state=false"),
         ],
         [
-            FilterOption(filterLabel: "chloe", filterUrlStr: nil),
-            FilterOption(filterLabel: "head", filterUrlStr: nil),
-            FilterOption(filterLabel: "sam", filterUrlStr: nil),
-            FilterOption(filterLabel: "zello", filterUrlStr: nil)
+            // 현재 assignee의 id로 필터링 가능, 중첩 사용은 안됌
+            FilterOption(filterLabel: "ghkdgus29", filterUrlStr: "assignee=ghkdgus29"),
+            FilterOption(filterLabel: "cire", filterUrlStr: "assignee=cire"),
         ],
         [
-            FilterOption(filterLabel: "레이블 없음", filterUrlStr: nil, isSelected: false),
-            FilterOption(filterLabel: "그룹프로젝트:이슈트래커", filterUrlStr: nil, isSelected: false)
+            FilterOption(filterLabel: "레이블 없음", filterUrlStr: nil),
+            FilterOption(filterLabel: "BE STEP1", filterUrlStr: nil),
         ]
     ]
     
     func collectSelectedFilterOptionUrlString() -> String {
+        // 필터 적용 형태 : 기본 URL + ?key1=value1&key2=value2
+        // labelNames의 경우 : 기본 URL + ?라벨이름키=라벨1,라벨2
         var collectedOptionUrlString = "?"
         
-        for options in list {
-            for option in options {
+        var urlString = ""
+        for index in 0...1 {
+            for option in list[index] {
                 guard let urlStr = option.filterUrlStr else { continue }
-                collectedOptionUrlString += option.isSelected ? urlStr : ""
+                collectedOptionUrlString += option.isSelected ? urlStr + "&" : ""
             }
         }
+        _ = urlString.popLast()
+        
+        var labelUrlString = ""
+        for option in list[2] {
+            guard let urlStr = option.filterUrlStr else { continue }
+            collectedOptionUrlString += option.isSelected ? urlStr + "," : ""
+        }
+        _ = labelUrlString.popLast()
+        
+        collectedOptionUrlString += urlString
+        collectedOptionUrlString += labelUrlString
+        
         return collectedOptionUrlString.count == 1 ? "" : collectedOptionUrlString
     }
 }
