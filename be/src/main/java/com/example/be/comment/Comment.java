9b5cf997a78c4f38ce694ebd291205f5b997be1a
Hyun <ghkdgus29@naver.com>
2023-05-26 13:54:49 +0900

    Feat: 댓글을 의미하는 Comment 엔티티 생성

---
+++ be/src/main/java/com/example/be/comment/Comment.java
@@ -0,0 +1,81 @@
+package com.example.be.comment;
+
+import com.example.be.issue.Issue;
+import com.example.be.user.User;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table("COMMENT")
+public class Comment {
+
+    @Id
+    private int id;
+
+    private String contents;
+    private LocalDateTime createdData;
+    private LocalDateTime lastUpdatedDate;
+    private boolean deleted;
+    private User user;
+    private Issue issue;
+
+    public Comment() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public LocalDateTime getCreatedData() {
+        return createdData;
+    }
+
+    public void setCreatedData(LocalDateTime createdData) {
+        this.createdData = createdData;
+    }
+
+    public LocalDateTime getLastUpdatedDate() {
+        return lastUpdatedDate;
+    }
+
+    public void setLastUpdatedDate(LocalDateTime lastUpdatedDate) {
+        this.lastUpdatedDate = lastUpdatedDate;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Issue getIssue() {
+        return issue;
+    }
+
+    public void setIssue(Issue issue) {
+        this.issue = issue;
+    }
+}
