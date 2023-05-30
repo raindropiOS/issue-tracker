46e0b30159a62392363a290f4611891ea4a74ddb
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-30 11:54:19 +0900

    Feat: URLRequestProducible 프로토콜 추가

--- iOS/issue-tracker/issue-tracker/Networking/URLRequestFactory.swift
+++ iOS/issue-tracker/issue-tracker/Networking/URLRequestFactory.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
-class URLRequestFactory {
+class URLRequestFactory: URLRequestProducible {
     func makeUrlRequest(_ urlString: String) -> URLRequest? {
         guard let url = URL(string: urlString) else { return nil }
         return URLRequest(url: url, timeoutInterval: 30.0)
     }
+}
+
+protocol URLRequestProducible {
+    func makeUrlRequest(_ urlString: String) -> URLRequest?
 }
