8932ecd0a1a504737c0356038293193b3e545f4e
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-05-31 15:51:35 +0900

    Feat: queryIssueData 메소드 추가
    - NetworkManager에서 쿼리를 받아 수행하는 메소드를 추가
    - baseUrlString 프라퍼티 추가

--- iOS/issue-tracker/issue-tracker/Networking/NetworkManager.swift
+++ iOS/issue-tracker/issue-tracker/Networking/NetworkManager.swift
@@ -12,6 +12,7 @@ class NetworkManager {
     let logger = Logger()
     let urlRequestFactory: URLRequestProducible = URLRequestFactory()
     let issueDataDecoder: IssueDataDecodable = IssueDataDecoder()
+    let baseUrlString: String = Server.base.rawValue
     
     func fetchIssueData(
         with urlString: String,
@@ -24,7 +25,7 @@ class NetworkManager {
                 if let error = error {
                     completion(.failure(error))
                 }
-
+                
                 if let data = data {
                     do {
                         let issueFrames = try self.issueDataDecoder.decodeIssueData(data)
@@ -35,4 +36,37 @@ class NetworkManager {
                 }
             }.resume()
         }
+    
+    func queryIssueData(
+        with query: [String: String],
+        completion: @escaping (Result<IssueFrameHolder, Error>) -> Void)
+    {
+        guard var urlComponents = URLComponents(string: baseUrlString) else {
+            logger.log("queryIssueData : URLComponents 생성 실패")
+            return
+        }
+        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+        urlComponents.queryItems = queryItems
+        guard let url = urlComponents.url else {
+            logger.log("queryIssueData : URL 생성 실패")
+            return
+        }
+        #warning("print문 삭제")
+        print(url)
+        let request = URLRequest(url: url, timeoutInterval: 30.0)
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            if let data = data {
+                do {
+                    let issueFrames = try self.issueDataDecoder.decodeIssueData(data)
+                    completion(.success(issueFrames))
+                } catch let decodeError {
+                    completion(.failure(decodeError))
+                }
+            }
+        }.resume()
+    }
 }
