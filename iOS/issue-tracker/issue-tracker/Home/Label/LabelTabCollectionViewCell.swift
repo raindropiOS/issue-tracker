16a16de5f225e49f2ffebed889eae7c8ad3a63f1
SONG <geun2121@gmail.com>
2023-05-31 20:09:48 +0900

    Feat: LabelTabCell 구현
    - Badge모양의 label에는 Issue목록에서 썼던 BadgeLabel을 사용
    - BadgeLabel의 구현방식 때문에 labelSlot :UIView위에 올리는 방식을 채택 
    - 재사용될때 중복문제 방지

--- iOS/issue-tracker/issue-tracker/Home/Label/LabelTabCollectionViewCell.swift
+++ iOS/issue-tracker/issue-tracker/Home/Label/LabelTabCollectionViewCell.swift
@@ -10,4 +10,88 @@ import SwipeCellKit
 
 class LabelTabCollectionViewCell: SwipeCollectionViewCell {
     static let identifier = "LabelTabCollectionViewCell"
+    private let hoizontalProportion: CGFloat = 24 / 375
+    private let verticalProportion: CGFloat = 16 / 84
+    private let labelSlot = UIView()
+    private var descriptionView = UILabel()
+    
+    var labelData: SingleLabel? {
+        didSet {
+            updateCell()
+        }
+    }
+    
+    override func layoutSubviews() { 
+        super.layoutSubviews()
+        setDescriptionView(descriptionView.text ?? "")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLabelSlot()
+        labelData = nil
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setLabelSlot()
+        labelData = nil
+    }
+    
+    func setDescriptionView(_ text: String) {
+        self.contentView.addSubview(descriptionView)
+        descriptionView.text = text
+        descriptionView.font = UIFont.systemFont(ofSize: round(labelSlot.frame.height * 0.5))
+        descriptionView.textColor = .gray
+        descriptionView.textAlignment = .left
+        descriptionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(
+            [
+                descriptionView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: round(self.contentView.frame.width * hoizontalProportion)),
+                descriptionView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: round(-self.contentView.frame.width * hoizontalProportion)),
+                descriptionView.topAnchor.constraint(equalTo: labelSlot.bottomAnchor),
+                descriptionView.heightAnchor.constraint(equalToConstant: round(self.contentView.frame.height * 0.33))
+            ]
+        )
+    }
+    
+    private func updateCell() {
+        labelSlot.subviews.forEach { $0.removeFromSuperview() }
+        
+        if let labelData = labelData {
+            let badge = BadgeLabel(name: labelData.name)
+            
+            if let description = labelData.description {
+                labelSlot.addSubview(badge)
+                descriptionView.text = description
+            } else {
+                labelSlot.addSubview(badge)
+            }
+            reconstraintsBadge(badge)
+        }
+    }
+    
+    private func setLabelSlot() {
+        
+        self.contentView.addSubview(labelSlot)
+        labelSlot.backgroundColor = UIColor.white
+        labelSlot.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                labelSlot.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: round(self.contentView.frame.width * hoizontalProportion)),
+                labelSlot.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: round(-self.contentView.frame.width * hoizontalProportion)),
+                labelSlot.topAnchor.constraint(equalTo: self.topAnchor, constant: round(self.contentView.frame.height * verticalProportion)),
+                labelSlot.heightAnchor.constraint(equalToConstant: self.contentView.frame.height * 0.33)
+            ]
+        )
+    }
+    private func reconstraintsBadge(_ badge: BadgeLabel) {
+        badge.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(
+            [
+                badge.leadingAnchor.constraint(equalTo: labelSlot.leadingAnchor),
+            ]
+        )
+    }
 }
