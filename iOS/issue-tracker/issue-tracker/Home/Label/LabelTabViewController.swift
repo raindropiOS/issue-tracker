786485e949ed9afab164f165e2b04b56414e0db1
SONG <geun2121@gmail.com>
2023-05-31 13:30:05 +0900

    Feat: LabelTabViewController생성

---
+++ iOS/issue-tracker/issue-tracker/Home/Label/LabelTabViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LabelTabViewController.swift
+//  issue-tracker
+//
+//  Created by SONG on 2023/05/29.
+//
+
+import UIKit
+
+class LabelTabViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
