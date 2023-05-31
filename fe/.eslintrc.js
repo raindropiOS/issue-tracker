858d162aa89f2b0d7e08627a64f5de3bbd7cff91
CiRe <dltpwns6@naver.com>
2023-05-31 11:00:13 +0900

    Feat: FE 자동 배포 브렌치 생성

--- fe/.eslintrc.js
+++ fe/.eslintrc.js
@@ -11,6 +11,8 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    "no-console": "off",
+
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
     'import/no-unresolved': 'off',
