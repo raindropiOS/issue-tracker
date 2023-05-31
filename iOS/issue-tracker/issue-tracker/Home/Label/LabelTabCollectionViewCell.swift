dac53c77b924feb53beb122d64b14c9da93e9a72
SONG <geun2121@gmail.com>
2023-05-31 13:35:20 +0900

    Feat: LabelTab Cell 생성

---
+++ iOS/issue-tracker/issue-tracker/Home/Label/LabelTabCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LabelTabCollectionViewCell.swift
+//  issue-tracker
+//
+//  Created by SONG on 2023/05/30.
+//
+
+import UIKit
+import SwipeCellKit
+
+class LabelTabCollectionViewCell: SwipeCollectionViewCell {
+    static let identifier = "LabelTabCollectionViewCell"
+}
