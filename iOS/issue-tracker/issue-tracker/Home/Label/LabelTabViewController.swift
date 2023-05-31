17bce07d82156393e82d09b250b7e5fc0bace6b2
SONG <geun2121@gmail.com>
2023-05-31 13:32:26 +0900

    Feat: LabelTab에 '추가'버튼 구현
    - navigationController를 스토리보드에서 임베드

--- iOS/issue-tracker/issue-tracker/Home/Label/LabelTabViewController.swift
+++ iOS/issue-tracker/issue-tracker/Home/Label/LabelTabViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class LabelTabViewController: UIViewController {
+    private var addButton: UIBarButtonItem!
+    private var collectionView: LabelTabCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAddButton()
     }
+    
+    private func setAddButton() {
+        addButton = UIBarButtonItem(title: "추가  ", style: .plain, target: self, action: #selector(addButtonTouched))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc private func addButtonTouched() {
+        
+    }
+    
 }
