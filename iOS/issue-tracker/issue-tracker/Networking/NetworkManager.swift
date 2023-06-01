f8fa529e0d769914add8f692b8c66cda37895a8e
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-02 05:12:49 +0900

    Feat: 페이징을 위한 네트워크 메니저 수정

--- iOS/issue-tracker/issue-tracker/Networking/NetworkManager.swift
+++ iOS/issue-tracker/issue-tracker/Networking/NetworkManager.swift
@@ -12,9 +12,12 @@ class NetworkManager {
     let logger = Logger()
     let urlRequestFactory: URLRequestProducible = URLRequestFactory()
     let issueDataDecoder: IssueDataDecodable = IssueDataDecoder()
+    let issueDataEncoder: IssueDataEncodable = IssueDataEncoder()
     let baseUrlString: String = Server.base.rawValue
-    let additionalInfoUrlString : String = Server.additionalInfo.rawValue
-    let additionalInformationDecoder : AdditionalInformationDataDecodable = AdditionalInformationDataDecoder()
+    let additionalInfoUrlString: String = Server.additionalInfo.rawValue
+    let additionalInformationDecoder: AdditionalInformationDataDecodable = AdditionalInformationDataDecoder()
+    var networkingState: NetworkingState = .fetchingDataEnded
+    var cntPage: Int = 1
     
     func fetchIssueData(
         with urlString: String,
@@ -23,11 +26,12 @@ class NetworkManager {
                 logger.log("Making URLRequest is failed")
                 return
             }
+            networkingState = .fetchingDataInProgress
             URLSession.shared.dataTask(with: request) { (data, _, error) in
                 if let error = error {
                     completion(.failure(error))
                 }
-                
+
                 if let data = data {
                     do {
                         let issueFrames = try self.issueDataDecoder.decodeIssueData(data)
@@ -36,6 +40,7 @@ class NetworkManager {
                         completion(.failure(decodeError))
                     }
                 }
+                self.networkingState = .fetchingDataEnded
             }.resume()
         }
     
@@ -46,6 +51,10 @@ class NetworkManager {
             logger.log("queryIssueData : URLComponents 생성 실패")
             return
         }
+            
+        var query = query
+        query["cntPage"] = "\(self.cntPage)"
+            
         let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
         urlComponents.queryItems = queryItems
         guard let url = urlComponents.url else {
@@ -53,6 +62,7 @@ class NetworkManager {
             return
         }
         let request = URLRequest(url: url, timeoutInterval: 30.0)
+        self.networkingState = .fetchingDataInProgress
         URLSession.shared.dataTask(with: request) { (data, _, error) in
             if let error = error {
                 completion(.failure(error))
@@ -61,11 +71,13 @@ class NetworkManager {
             if let data = data {
                 do {
                     let issueFrames = try self.issueDataDecoder.decodeIssueData(data)
+                    self.cntPage += 1
                     completion(.success(issueFrames))
                 } catch let decodeError {
                     completion(.failure(decodeError))
                 }
             }
+            self.networkingState = .fetchingDataEnded
         }.resume()
     }
     
@@ -75,6 +87,7 @@ class NetworkManager {
                 logger.log("Making URLRequest is failed")
                 return
             }
+            networkingState = .fetchingDataInProgress
             URLSession.shared.dataTask(with: request) { (data, _, error) in
                 if let error = error {
                     completion(.failure(error))
@@ -83,14 +96,43 @@ class NetworkManager {
                 if let data = data {
                     do {
                         let additionalInfo = try self.additionalInformationDecoder.decodeIssueData(data)
-                        print(additionalInfo)
                         completion(.success(additionalInfo))
                     } catch let decodeError {
                         completion(.failure(decodeError))
                     }
                 }
+                self.networkingState = .fetchingDataEnded
             }.resume()
         }
+
+    func postIssue(with issue: PostIssueFormat) {
+        let urlString = Server.additionalInfo.rawValue
+        guard var request = urlRequestFactory.makeUrlRequest(urlString) else {
+            logger.log("Making URLRequest is failed")
+            return
+        }
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let data = issueDataEncoder.encodeIssueDatum(with: issue) else { return }
+        print(String(data: data, encoding: .utf8))
+        let jsonData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? Data ?? nil
+        
+        request.httpBody = data
+        
+        networkingState = .fetchingDataInProgress
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response {
+                print("response : \(response)")
+            }
+            
+            if let error = error {
+                print("error : \(error)")
+            }
+            self.networkingState = .fetchingDataEnded
+        }.resume()
+    }
+}
+
 enum NetworkingState {
     case fetchingDataInProgress
     case fetchingDataEnded
