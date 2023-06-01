4da61e5dfae532254176002101559f94c33c582b
SONG <geun2121@gmail.com>
2023-06-01 21:21:26 +0900

    Feat : 마일스톤, 내계정 탭 UI 변경

--- iOS/issue-tracker/issue-tracker/Home/Issue/Views/AddIssueTableViewCell.swift
+++ iOS/issue-tracker/issue-tracker/Home/Issue/Views/AddIssueTableViewCell.swift
@@ -19,7 +19,7 @@ class AddIssueTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
